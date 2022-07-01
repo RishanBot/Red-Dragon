5be1e0f9f32e8576872b13943f9e416422c03d27
RishanBot <108547698+RishanBot@users.noreply.github.com>
2022-07-02 00:23:40 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -22,20 +22,20 @@ global.APIKeys = {
 global.alivemsg = `My name is RED DRAGON🔥
 My Creator is Kumuthu Prabhasha😎` //Costomize Alive Message (type your message in `` )
 
-global.owner = ['+380944814219'] //Owner number in aive msg
-global.premium = ['+380944814219'] //Owner Number info
-global.ownernomer = '+380944814219' //Owner Number <<<
+global.owner = ['+94741475585'] //Owner number in aive msg
+global.premium = ['+94741475585'] //Owner Number info
+global.ownernomer = '+94741475585' //Owner Number <<<
 
 global.ownername = '😎𝙆.𝙋𝙧𝙖𝙗𝙝𝙖𝙨𝙝𝙖' //Owner Name
 global.botname = ' ☬RED༒DRAGON' //Bot Name
 
-global.button = '🤭මේක ටච් කරන්න📍' //Costomize A Button Name In Alive Message
-global.btnurl = '🙂🔥දැන් සැපද?😑' //Change Your Button's URL
+global.button = '🤭මේක ටච් එපා හුත්තො📍' //Costomize A Button Name In Alive Message
+global.btnurl = '🙂🔥හුකා ගන්නැතුව පලයන්?😑' //Change Your Button's URL
 
 global.region = 'India, South India, Kerala' //Country Region
 
-global.packname = '☬RED༒DRAGON࿐' //Bot Pacage Name
-global.author = '😎𝙆.𝙋𝙧𝙖𝙗𝙝𝙖𝙨𝙝𝙖❯❯' //Author Name
+global.packname = 'ʀ ɪ s ʜ ᴀ ɴ ʙᴏᴛ' //Bot Pacage Name
+global.author = '😎ʀ ɪ s ʜ ᴀ ɴ❯❯' //Author Name
 global.prefa = ['','!','.','😂','❤','💠'] //Command Prefix eg:(.alive ,alive 😂alive)
 global.sessionName = 'session' //Session Name!
 global.sp = '⭔' //A Charactor In Menu!
@@ -43,9 +43,9 @@ global.footer = '©_Kumuthu.' //In list menu
 global.chatbotpre = false  // Chat bot true/false
 
 
-global.ig = 'https://github.com/prabhasha2006' //Github Account!
+global.ig = 'https://www.facebook.com/profile.php?id=100073958067407' //Facebook Account!
 
-global.mess = { // Error Mesages
+global.mess = { // ʀ ɪ s ʜ ᴀ ɴ
     success: 'Done ✅',
     admin: 'Only Admins can use it!😉',
     botAdmin: 'Bot wants to be an Admin😥',
